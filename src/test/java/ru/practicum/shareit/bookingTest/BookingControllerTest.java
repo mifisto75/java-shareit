1934263a73421cc5008603c2a0ecea1e1b9917bf
Николай баранович <nick.fakov@yandex.ru>
2023-08-29 00:16:30 +0300

    #12

--- src/test/java/ru/practicum/shareit/bookingTest/BookingControllerTest.java
+++ src/test/java/ru/practicum/shareit/bookingTest/BookingControllerTest.java
@@ -157,43 +157,94 @@ public class BookingControllerTest {
     @Test
     void positiveGetAllBookingOneUser() throws Exception {
         mockMvc.perform(
-                post("/bookings")
-                        .content("{\n" +
-                                "    \"itemId\": 1,\n" +
-                                "    \"start\": \"2023-11-11T11:11:01\",\n" +
-                                "    \"end\": \"2023-11-11T11:11:02\"\n" +
-                                "}")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("X-Sharer-User-Id", 2)
-        ).andExpect(status().isOk());
-
-        mockMvc.perform(
                 get("/bookings")
                         .contentType(MediaType.APPLICATION_JSON)
                         .header("X-Sharer-User-Id", 1)
         ).andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(
+                get("/bookings?state=CURRENT")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(
+                get("/bookings?state=PAST")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(
+                get("/bookings?state=FUTURE")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(
+                get("/bookings?state=WAITING")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(
+                get("/bookings?state=REJECTED")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+
     }
 
+    @Test
+    void negativeGetAllBookingOneUser() throws Exception {
+
+        mockMvc.perform(
+                get("/bookings?state=rrrrr")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is4xxClientError());
+    }
 
     @Test
     void positiveGetAllBookingOneOwner() throws Exception {
-        mockMvc.perform(
-                post("/bookings")
-                        .content("{\n" +
-                                "    \"itemId\": 1,\n" +
-                                "    \"start\": \"2023-11-11T11:11:01\",\n" +
-                                "    \"end\": \"2023-11-11T11:11:02\"\n" +
-                                "}")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("X-Sharer-User-Id", 2)
-        ).andExpect(status().isOk());
-
         mockMvc.perform(
                 get("/bookings/owner")
                         .contentType(MediaType.APPLICATION_JSON)
                         .header("X-Sharer-User-Id", 1)
         ).andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(
+                get("/bookings/owner?state=CURRENT")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+        mockMvc.perform(
+                get("/bookings/owner?state=PAST")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+        mockMvc.perform(
+                get("/bookings/owner?state=FUTURE")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+        mockMvc.perform(
+                get("/bookings/owner?state=WAITING")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
+        mockMvc.perform(
+                get("/bookings/owner?state=REJECTED")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is2xxSuccessful());
     }
 
-
+    @Test
+    void negativeGetAllBookingOneOwner() throws Exception {
+        mockMvc.perform(
+                get("/bookings?state=rrrrr")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1)
+        ).andExpect(status().is4xxClientError());
+    }
 }
