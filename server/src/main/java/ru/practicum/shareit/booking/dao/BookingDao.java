0fccb9fad0f6c57ac3f2d1acfd4b67b46dba1193
Николай баранович <nick.fakov@yandex.ru>
2023-09-09 19:48:26 +0300

    #beta

--- server/src/main/java/ru/practicum/shareit/booking/dao/BookingDao.java
+++ server/src/main/java/ru/practicum/shareit/booking/dao/BookingDao.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.dao;
 
 import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
